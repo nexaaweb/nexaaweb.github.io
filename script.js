2e58c631f068e6d5ca978b92cfe13a931d004b22
nexaaweb <nexaawebofficial@gmail.com>
2026-02-26 12:37:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener("load", () => {
+  const hero = document.querySelector(".hero-content");
+  hero.style.opacity = "0";
+  hero.style.transform = "translateY(50px)";
 
+  setTimeout(() => {
+    hero.style.transition = "all 1s ease";
+    hero.style.opacity = "1";
+    hero.style.transform = "translateY(0)";
+  }, 300);
+});
