5c1ab76b3bea1d46270d3d827693591b3d5bedb5
nexaaweb <nexaawebofficial@gmail.com>
2026-03-11 16:02:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,37 @@ const navLinks = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// MOBILE MENU TOGGLE
+
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+
+// TESTIMONIALS AUTO SLIDER
+
+let testimonials = document.querySelectorAll(".testimonial-card");
+let index = 0;
+
+function showTestimonial() {
+
+  testimonials.forEach(card => {
+    card.classList.remove("active");
+  });
+
+  index++;
+
+  if (index >= testimonials.length) {
+    index = 0;
+  }
+
+  testimonials[index].classList.add("active");
+
+}
+
+setInterval(showTestimonial, 4000);
