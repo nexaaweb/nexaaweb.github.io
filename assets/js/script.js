cc307f1d9e7af3c7372655197696d44a5fdc32a0
nexaaweb <nexaawebofficial@gmail.com>
2026-03-13 16:06:19 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,1 @@
-/* HEADER LOAD */
-
-fetch("components/header.html")
-.then(res => res.text())
-.then(data => {
-document.getElementById("header").innerHTML = data;
-});
-
-/* FOOTER LOAD */
-
-fetch("components/footer.html")
-.then(res => res.text())
-.then(data => {
-document.getElementById("footer").innerHTML = data;
-});
+console.log("NexaaWeb website loaded");
