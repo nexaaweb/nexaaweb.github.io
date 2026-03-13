71bfea358d0bdc7c12fea524830124de7c0a7a0c
nexaaweb <nexaawebofficial@gmail.com>
2026-03-13 21:11:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 console.log("NexaaWeb website loaded");
+
+function updateDate(){
+const options={
+weekday:"long",
+year:"numeric",
+month:"long",
+day:"numeric"
+};
+
+const today=new Date().toLocaleDateString("en-US",options);
+
+document.getElementById("date").textContent=today;
+}
+
+updateDate();
