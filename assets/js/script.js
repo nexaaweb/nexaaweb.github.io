2016626790e78ceea863911c36bc832700f87cd7
nexaaweb <nexaawebofficial@gmail.com>
2026-03-14 14:14:37 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 fetch("components/header.html")
-.then(res => res.text())
-.then(data => {
-document.getElementById("header").innerHTML = data;
-});
+.then(res=>res.text())
+.then(data=>{
+document.getElementById("header").innerHTML=data;
+})
 
 fetch("components/footer.html")
-.then(res => res.text())
-.then(data => {
-document.getElementById("footer").innerHTML = data;
-});
+.then(res=>res.text())
+.then(data=>{
+document.getElementById("footer").innerHTML=data;
+})
 
 
 function updateDate(){
@@ -18,12 +18,12 @@ weekday:"long",
 year:"numeric",
 month:"long",
 day:"numeric"
-};
+}
 
-const today=new Date().toLocaleDateString("en-US",options);
+const today=new Date().toLocaleDateString("en-US",options)
 
-document.getElementById("date").innerText=today;
+document.getElementById("date").innerText=today
 
 }
 
-updateDate();
+updateDate()
